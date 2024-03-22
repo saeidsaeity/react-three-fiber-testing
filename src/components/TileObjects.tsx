4ceee59e134689d989c82d358f810a128bf22438
saeidsaeity <saeidsaeity@outlook.com>
2024-03-22 10:48:44 +0000

    glows

--- src/components/TileObjects.tsx
+++ src/components/TileObjects.tsx
@@ -2,19 +2,21 @@ import { useLoader } from '@react-three/fiber';
 
 import React, { useRef, useState } from 'react'
 import * as THREE from 'three';
-import { useGLTF,DragControls } from '@react-three/drei'
+import { useGLTF,DragControls} from '@react-three/drei'
 import { GLTFLoader } from 'three/addons';
 import { PropsWithChildren } from 'react'
 import {views} from 'react-three/drei'
 import { Suspense } from 'react';
 import { RigidBody } from '@react-three/rapier';
 import { Mesh, ShaderMaterial } from 'three';
+import { Selection, Select, EffectComposer, Outline } from '@react-three/postprocessing'
 
 function TileObjects(props:PropsWithChildren) {
     const tileA = useLoader(GLTFLoader,'/monastery.gltf')
     const emptytile=[]
     const [selectedTile, setSelectedTile] = useState<any>(null);
     const offset = useRef<THREE.Vector3>(new THREE.Vector3());
+    const [hovered, setHovered] = useState(false);
     const createGlowMaterial = () => {
       return new THREE.MeshBasicMaterial({
         color: 0xffff00,
@@ -23,24 +25,21 @@ function TileObjects(props:PropsWithChildren) {
       });
     };
    
-
+    const tileRefs = useRef([]);
     for(let x=0;x<4;x++){
       for(let y=0;y<4;y++){
-        const tileRef = useRef<THREE.Mesh>(null);
-        const glowRef = useRef<THREE.Mesh>(null);
         
-        console.log(tileRef);
+     
+        
+        
         
         emptytile.push(
         <RigidBody type="dynamic" >
-          <DragControls onHover={(hovering)=>{
-            console.log(tileA.scene.clone().children[0].material);
-            console.log(tileRef.current.material);
-            if(hovering && tileRef.current){tileRef.current.material = createGlowMaterial()}
-            else if(!hovering && tileRef.current){tileRef.current.material = tileA.scene.clone().children[0].material}}} key={`Rigid-${x}-${y}`} >
-
-              <primitive key={`${x}-${y}`} ref={tileRef} object={tileA.scene.clone()} position={[x * 2.5, 0, y * 2.5]} />
-              
+          <DragControls key={`Rigid-${x}-${y}`} >
+              <Select enabled={hovered}>
+              <primitive ref={tileRefs.current[x]} onPointerOver={(event) => {console.log(event); setHovered(true)}}
+      onPointerOut={(event) => {console.log(event); setHovered(false)}} key={`${x}-${y}`} object={tileA.scene.clone()} position={[x * 2.5, 0, y * 2.5]} />
+            </Select>
           </DragControls>
           </RigidBody>)
         
@@ -52,7 +51,12 @@ function TileObjects(props:PropsWithChildren) {
       <>
       <ambientLight/>
       
-      <Suspense>{emptytile}</Suspense>
+      <Selection>
+        <EffectComposer multisampling={8} autoClear={false}>
+          <Outline blur visibleEdgeColor="white" edgeStrength={100} width={1000} />
+          </EffectComposer>
+          {emptytile}
+          </Selection>
       
     
       
